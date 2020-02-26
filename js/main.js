e37d28b495cc38467c0af5d00fdf423ac2bf3724
Hayao Gai <hayao.cheng@o-pa.com.tw>
2020-02-26 16:46:45 +0800

    Optimize script #2

--- js/main.js
+++ js/main.js
@@ -3,29 +3,21 @@
 (function () {
 	'use strict';
 	
+	// UI 參數
+	const number = "0.0.1"; //版本號
+	const size = 56; //按鈕圖片的尺寸（準備的圖也要是這個尺寸）
+	const distance = 30; //按鈕離邊界的距離
+
+	// UI 介面
+	let black, camera, version;
+
+	// 當前螢幕大小
+	let width, height;
+
+
 	window.onload = () => {
-
+		addUI();
 		requestAnimationFrame(onUpdate);
-    
-		const marker = document.querySelectorAll("#marker")[0];
-		const model = document.querySelectorAll("#model")[0];
-		const canvas = document.querySelectorAll(".a-canvas")[0];
-	    const hammer = new Hammer(canvas);
-		const audio = new Audio("audio/generate.mp3");
-		
-		const c1 = window.innerWidth * 0.25;
-		const c2 = window.innerHeight * 0.25;
-		let press = 0;
-		
-		const max = 5;
-		const min = 1;
-		const speed = 0.3;
-		let scale = 1;
-
-	    detectAR(marker, model, audio);
-		defineGesture(hammer);
-		dragModel(model, hammer, c1, c2, press);
-	    pinchModel(model, hammer, scale, max, min, speed);
 	};
 	
 
@@ -33,90 +25,70 @@
 	function onUpdate(time) {
 	    requestAnimationFrame(onUpdate);
 		TWEEN.update(time);
+
+		// 更新螢幕大小
+		if (width === window.innerWidth && height === window.innerHeight) return;
+		width = window.innerWidth;
+		height = window.innerHeight;
+		setUI();
 	}
 
-	function detectAR(marker, model, audio) {
-
-		// 偵測掃描卡
-		marker.addEventListener("markerFound", () => {
-
-	        model.setAttribute("position", "0 0 0");
-			model.setAttribute("scale", "0 0 0");
-			audio.play();
-
-			// 在 750 毫秒內，將 s 補間動畫至 { 3, 3, 3 }
-			let s = { x: 0, y: 0, z: 0};
-			new TWEEN.Tween(s)
-			.to({ x: 3, y: 3, z: 3 }, 750)
-			.easing(TWEEN.Easing.Back.Out)
-			.onUpdate(() => model.setAttribute("scale", `${s.x} ${s.y} ${s.z}`)) // 每幀變化時進行的動作
-			.delay(750) // 延遲 750 毫秒再進行下一步
-			.start(); // 開始執行
-		});
-
-		// 遺失掃描卡
-	    marker.addEventListener("markerLost", () => {
-			audio.pause();
-			audio.currentTime = 0; 
-		});
-	}
-
-	function defineGesture(hammer) {
-		hammer.get('pan').set("direction", "Hammer.DIRECTION_ALL");
-		hammer.add(new Hammer.Pinch());
-	}
-
-	function dragModel(model, hammer, c1, c2, press) {
-
-		hammer.on("panstart", e => {
-
-			// 以迴歸分析計算 螢幕座標 → 模型座標
-			const x = e.center.x / c1 - 2;
-			const z = e.center.y / c2 - 2;
-			let origin = model.getAttribute("position");
-			press = 0;
-			
-			// 在 100 毫秒內，將 origin 補間動畫至 { x, 0, z }
-			new TWEEN.Tween(origin)
-			.to({ x: x, z: z }, 100)
-			.easing(TWEEN.Easing.Quadratic.Out)
-			.onUpdate(() => model.setAttribute("position", `${origin.x} 0 ${origin.z}`)) // 每幀變化時進行的動作
-			.onComplete(() => press++) // 完成後 press + 1
-			.start(); // 開始執行
-		});
+	function addUI() {
 		
-		hammer.on("panmove", e => {
-	        if (press < 1) return;
-			const x = e.center.x / c1 - 2;
-			const z = e.center.y / c2 - 2;
-			model.setAttribute("position", `${x} 0 ${z}`);
-		});
+		// Black
+		black = document.createElement("div");
+        black.style.backgroundColor = "black";
+		black.style.position = "fixed";
+        document.body.appendChild(black);
+		
+		// Camera
+		camera = document.createElement("div");
+        camera.style.backgroundImage = "url('image/camera.png')";
+        camera.style.width = `${size}px`;
+        camera.style.height = `${size}px`;
+        camera.style.position = "fixed";
+        document.body.appendChild(camera);
+
+		// Version
+        version = document.createElement("div");
+        version.style.color = "white";
+        version.style.position = "fixed";
+        version.style.right = "10px";
+        version.style.bottom = "10px";
+        version.innerHTML = `version_${numbrt}`;
+        document.body.appendChild(version);
 	}
 
-	function pinchModel(model, hammer, scale, max, min, speed) {
+	function setUI() {
+		
+		// 參數重置
+        reset(black);
+		reset(camera);
 
-		hammer.on("pinchstart", () => scale = 1);
+		if (width < height) {
+			// 直式螢幕
+            black.style.width = `${width}px`;
+            black.style.height = `${distance * 2 + size}px`;
+            black.style.left = "0px";
+            black.style.bottom = "0px";
+            camera.style.left = `${width * 0.5 - size * 0.5}px`;
+            camera.style.bottom = `${distance}px`;
+        } else {
+			// 橫式螢幕
+            black.style.width = `${distance * 2 + size}px`;
+            black.style.height = `${height}px`;
+            black.style.right = "0px";
+            black.style.top = "0px";
+            camera.style.right = `${distance}px`;
+            camera.style.top = `${height * 0.5 - size * 0.5}px`;
+        }
+	}
 
-		hammer.on("pinchmove", e => {
-
-	        if (e.scale === scale) return;
-
-			else if (e.scale > scale) {
-				// 放大
-	            scale = e.scale;
-				let s = model.getAttribute("scale").x;
-				if (s >= max) return;
-				s += speed;
-				model.setAttribute("scale", `${s} ${s} ${s}`);
-			} else {
-				// 縮小
-	            scale = e.scale;
-				let s = model.getAttribute("scale").x;
-				if (s <= min) return;
-				s -= speed;
-				model.setAttribute("scale", `${s} ${s} ${s}`);
-			}
-		});
+	function reset(ui) {
+		ui.style.left = null;
+        ui.style.right = null;
+        ui.style.top = null;
+        ui.style.bottom = null;
 	}
 
 })();
